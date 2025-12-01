278708caeb22fc0ef267e9183ab3b4159a071e65
nbahrom123-spec <nbahrom123@gmail.com>
2025-12-02 02:11:20 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -6,8 +6,8 @@ export default async function handler(req) {
   const country = req.headers.get("x-vercel-ip-country") || "XX";
 
   if (country === "RU") {
-    return Response.redirect("https://newwebl.pro/client.php?flow=254249-271444-1", 302);
+    return Response.redirect("https://newwebl.pro/client.php?flow=254249-271966-1", 302);
   }
 
-  return Response.redirect("https://tone.affomelody.com/0wn8qi", 302);
+  return Response.redirect("https://sisuny.top/click?o=2&a=1551", 302);
 }

